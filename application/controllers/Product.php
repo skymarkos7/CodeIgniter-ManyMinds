f29143aac36d1fc40680d09c504c7b9ec7c5ac9b
marcos <marcos@slice.global>
2023-03-17 18:02:54 -0300

    clear

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -7,8 +7,12 @@ class Product extends CI_Controller {
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
 	public function index()	{
-		
-		$this->load->view('products/productView');	
+		$this->load->model("ProductModel", "ProductModel");
+
+		$result = $this->ProductModel->list_product();
+		$data   = array("ProductModel" => $result);
+
+		$this->load->view('products/productView', $data);	
 
 	}
 
--- application/models/ProductModel.php
+++ application/models/ProductModel.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class LoginModel extends CI_Model {
+class ProductModel extends CI_Model {
 
-	public function list_user() {
+	public function list_product() {
 		$this->db->select("user", "pass", "level_acess");
 
 		$result = $this->db->get("colaborador")->result();
