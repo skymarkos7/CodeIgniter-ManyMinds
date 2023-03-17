0d871dd6f926d92e43db2aabaa8fae2f0fb9c3a2
marcos <marcos@slice.global>
2023-03-17 17:07:24 -0300

    corrigindo login

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,39 +1,18 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+defined('BASEPATH') or exit('No direct script access allowed');
 
-	public function index()	{
-		
-		session_start(); //inicia uma sessão
+class Welcome extends CI_Controller
+{
+    public function index()
+    {
+        session_start(); //inicia uma sessão
 
-		if(isset($_SESSION['id']) && empty($_SESSION['id']) == false)   {  //verifica se já foi feito login recentemente e mostra o html a baixo "pagina principal do site"
-
-		//inicio do site
-			echo " 
-
-			<!DOCTYPE html>
-			<html>
-			<head>
-				<title>Marcola</title>
-			</head>
-			<body>
-				
-				<div>site aqui</div>
-
-			</body>
-			</html>
-
-			";		
-
-} else {
-
-	header("Location:index.php/login/index"); // leva o usuário até a página de login caso ele não esteja logado
-
-}
-
-
-
-		
-	}
+        if (isset($_SESSION['id']) && empty($_SESSION['id']) == false) {  //verifica se já foi feito login recentemente e mostra o html a baixo "pagina principal do site"
+            header("Location:index.php/login/index"); // leva o usuário até a página de login caso ele não esteja logado
+        } else {
+           
+			header("Location:index.php/dasboard/index");
+        }
+    }
 }
