105a9d282f3cb1a64796b843ecb814d7d0a0c57c
marcos <marcos@slice.global>
2023-03-20 05:44:31 -0300

    sudindo banco

--- Docs/manyDB.sql
+++ Docs/manyDB.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `colaborador` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) DEFAULT NULL,
   `pass` varchar(50) DEFAULT NULL,
-  `level_acess` int(1) DEFAULT NULL,
+  `level_acess` varchar(20) DEFAULT NULL,
   `usuario_fornecedor` varchar(20) DEFAULT NULL,
   `ativo_inativo` varchar(20) DEFAULT NULL,
   `nome_colaborador` varchar(50) DEFAULT NULL,
@@ -34,21 +34,26 @@ CREATE TABLE IF NOT EXISTS `colaborador` (
   `cep` varchar(10) DEFAULT NULL,
   `rua` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Copiando dados para a tabela many_minds.colaborador: ~6 rows (aproximadamente)
+-- Copiando dados para a tabela many_minds.colaborador: ~7 rows (aproximadamente)
 INSERT INTO `colaborador` (`id`, `user`, `pass`, `level_acess`, `usuario_fornecedor`, `ativo_inativo`, `nome_colaborador`, `data_cadastro`, `cidade`, `estado`, `cep`, `rua`) VALUES
-	(1, 'jose@ativo.com', 'jose@ativo.usuario', 1, 'usuario', 'ativo', 'jose da silva', '16/03/2023', 'boca da mata', 'AL', '57680000', 'manoe cavalcantel '),
 	(2, NULL, NULL, NULL, 'fornecedor', 'inativo', 'coca-cola', '10/02/2023', 'recife', 'CE', '55668422', 'tereaz manira '),
-	(3, 'manoel@ativo.com', 'manoel@ativo.com', 2, 'usuario', 'ativo', 'monel gomes', '05/03/2022', 'são paulo', 'SP', '55546698', 'ginval de alcantara'),
-	(4, 'josefa@inativo.com', 'josefa@inativo.com', 1, 'usuario', 'inativo', 'josefa mainarte', '03/03/2023', 'belo horizonte', 'MG', '55642265', 'texeira suvial'),
 	(5, NULL, NULL, NULL, 'fornecedor', 'ativo', 'P&G', '15/01/2020', 'são paulo', 'SP', '11332545', 'manguezal craibas'),
-	(6, 'marcos@teste.com', 'root', 1, 'usuario', 'ativo', 'marcos homologacao', '01/01/2000', 'são miguel', 'AL', '57669554', 'bom jesus dos milagres');
+	(6, 'marcos@teste.com', 'cm9vdA==', 'Administrador', 'usuario', 'ativo', 'marcos homologacao', '01/01/2000', 'são miguel', 'AL', '57669554', 'bom jesus dos milagres'),
+	(33, 'marcosrealbmata@gmail.com', 'MzIx', 'Administrador', 'usuario', NULL, 'marcos lourenço', '2023-03-01', 'boca da mata', 'Alagoas', '57680000', 'alecanar'),
+	(34, 'master@gmail.com', 'bWFzdGVy', 'Administrador', 'usuario', NULL, 'zeca', '2022-02-01', 'bm', 'CE', '55546288', 'santa terezinha'),
+	(35, 'comum@gmail.com', 'Y29tdW0=', 'Nível Comum', 'usuario', NULL, 'zezinho', '2022-02-01', 'bm', 'SP', '66321555', 'praça da vitoria'),
+	(52, NULL, NULL, NULL, 'fornecedor', 'ativo', 'marcos lourenço', '20/20/2020', 'boca da mata', 'Alagoas', '57680000', 'manoel cavalcante');
 
 -- Copiando estrutura para tabela many_minds.logs
 CREATE TABLE IF NOT EXISTS `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `json` longtext NOT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `tentativas` int(11) DEFAULT NULL,
+  `os` varchar(50) DEFAULT NULL,
+  `data` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -58,16 +63,22 @@ CREATE TABLE IF NOT EXISTS `logs` (
 CREATE TABLE IF NOT EXISTS `pedidos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fornecedor_produto` varchar(50) DEFAULT NULL,
-  `observacao` varchar(50) DEFAULT NULL,
+  `observacao` longtext DEFAULT NULL,
   `ativo_finalizado` varchar(20) DEFAULT NULL,
   `quantidade` varchar(5) DEFAULT NULL,
   `preco_unitario` varchar(5) DEFAULT NULL,
+  `produto` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Copiando dados para a tabela many_minds.pedidos: ~1 rows (aproximadamente)
-INSERT INTO `pedidos` (`id`, `fornecedor_produto`, `observacao`, `ativo_finalizado`, `quantidade`, `preco_unitario`) VALUES
-	(1, 'P&G', 'fio dental', 'ativo', '1', '5,60');
+-- Copiando dados para a tabela many_minds.pedidos: ~5 rows (aproximadamente)
+INSERT INTO `pedidos` (`id`, `fornecedor_produto`, `observacao`, `ativo_finalizado`, `quantidade`, `preco_unitario`, `produto`) VALUES
+	(1, 'P&G', 'fio dental', 'ativo', '1', '5,60', 'fio dental 5m'),
+	(2, 'rolex', 'vendedor de relogios', 'ativo', '2', '10000', 'relógio'),
+	(3, 'jhonson&jhnson', 'mandar tamanho grande', 'finalizado', '200', '22,30', 'fralda'),
+	(4, 'jhonson&jhnson', 'mandar tamanho pequeno', 'ativo', '60', '18,90', 'fralda'),
+	(5, 'jhonson&jhnson', 'mandar sortido', 'ativo', '2', '22,30', 'fralda'),
+	(6, 'logitec', 'alta qualidade', 'ativo', '5', '180', 'mouse');
 
 -- Copiando estrutura para tabela many_minds.produtos
 CREATE TABLE IF NOT EXISTS `produtos` (
@@ -76,13 +87,15 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `fornecedor_produto` varchar(50) DEFAULT NULL,
   `ativo_inativo` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Copiando dados para a tabela many_minds.produtos: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela many_minds.produtos: ~5 rows (aproximadamente)
 INSERT INTO `produtos` (`id`, `nome_produto`, `fornecedor_produto`, `ativo_inativo`) VALUES
-	(1, 'escova de dentes', 'P&G', 'ativo'),
+	(1, 'escova de dentes', 'P&G', 'inativo'),
 	(2, 'fio dental', 'P&G', 'inativo'),
-	(3, 'fanta 2 L', 'coca-cola', 'ativo');
+	(3, 'fanta 2 L', 'coca-cola', 'ativo'),
+	(4, 'pepsi ks', 'pepsi', 'ativo'),
+	(5, 'açucar ', 'caétes', 'ativo');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
